41b26dc8163e82c167877e0b106a2a9afb27a605
Ayan02-coder <98633937+Ayan02-coder@users.noreply.github.com>
2023-09-11 10:57:33 +0530

    Update Caesar-cipher.cpp

--- Caesar-cipher.cpp
+++ Caesar-cipher.cpp
@@ -1,16 +1,33 @@
-//RBT21CB021
 #include <iostream>
 using namespace std;
 
-string encry(string t, int shift)
+string encrypt(string text, int shift)
 {
-	string result = "";
-    for (int i = 0; i < t.length(); i++) {
-		if (isupper(t[i]))
-			result += char(int(t[i] + shift - 65) % 26 + 97);
-			
+	string result;
+	result = "";
+    for (int i = 0; i < text.length(); i++) {
+		if (isupper(text[i]))
+			//result += char(int(t[i] + shift - 65) % 26 + 65);
+			result += char(int(text[i] + shift - 65) % 26 + 97);
 		else
-			result += char(int(t[i] + shift - 97) % 26 + 65);
+			//result += char(int(t[i] + shift - 97) % 26 + 97);
+			result += char(int(text[i] + shift - 97) % 26 + 65);
+	}
+
+	return result;
+}
+
+string decrypt(string a, int shift)
+{
+	string result;
+	result = "";
+    for (int i = 0; i < a.length(); i++) {
+		if (isupper(a[i]))
+		
+			result += char(int(a[i] - shift - 65) % 26 + 97);
+		else
+		
+			result += char(int(a[i] - shift - 97) % 26 + 65);
 	}
 
 	return result;
@@ -19,9 +36,17 @@ string encry(string t, int shift)
 
 int main()
 {
-	string t = "ENGINEERING";
-	int shift = 3;
-	cout << "Text : " << t;
-	cout << "\nCipher: " << encry(t, shift);
+	string text,a;
+	int shift;
+	cout << "Enter a Text here: ";
+	cin >> text;
+	cout << "Enter a Shift count: ";
+	cin >> shift;
+	cout << "Text : " << text;
+	a = encrypt(text,shift);
+	cout << "\nCipher: " << a;
+	cout << "\nMain text: "<< decrypt(a,shift);
+	
+	
 	return 0;
 }
